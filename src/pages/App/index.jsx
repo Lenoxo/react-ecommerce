06e0a0f9ab77500dc6db8a8a4682c7c9b5b4a4c5
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-30 17:02:33 -0500

    Integro el uso de useRoutes y BrowserRouter con react-router-dom. Avances hasta la clase 4.

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,22 +1,31 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import NotFound from '../NotFound'
 import './index.css'
+
+const AppRoutes = () => {
+  // Para las rutas con useRoutes, es necesario guardar dentro de un array objetos con esta sintaxis.
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/MyAccount', element: <MyAccount /> },
+    { path: '/MyOrder', element: <MyOrder /> },
+    { path: '/MyOrders', element: <MyOrders /> },
+    { path: '/SignIn', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ])
+  
+  return routes
+}
 
 function App() {
   return (
-    <div className='bg-slate-400'>
-      Hola mundo
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
