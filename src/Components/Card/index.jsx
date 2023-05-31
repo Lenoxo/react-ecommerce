99077cabdf58a2ab729d739f45f940aa626a41a2
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-30 22:40:47 -0500

    Añado la API, renderizo de forma dinámica Cards. Avances hasta la parte 8.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,23 +1,23 @@
-function Card() {
+function Card({ category, title, image, price }) { // Estos datos los recibo por props.
   return (
     <div className="bg-white cursor-pointer w-56 h-68 rounded-lg">
-      <figure className="relative mb-2 w-full h-4/5 rounded-lg">
+    <figure className="relative mb-2 w-full h-4/5 rounded-lg">
         <span className="absolute bottom-0 left-0 bg-zinc-300 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          Electronics
+        {category}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="http://t0.gstatic.com/images?q=tbn:ANd9GcTIHAwI9f7-kNF6xsPR573FXecUF5HLJMveyeMuHEZG4z_c7Cg&s"
-          alt="Headphones"
+          src={image}
+          alt={title}
         />
         <button className="absolute top-0 right-0 flex justify-center items-center rounded-full bg-white m-2 p-1 w-6 h-6">
-          <p>+</p>
+        <p>+</p>
         </button>
-      </figure>
-      <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lm font-bold">$300</span>
-      </p>
+    </figure>
+    <p className="flex justify-between">
+        <span className="text-sm font-light">{title}</span>
+        <span className="text-lm font-bold">${price}</span>
+    </p>
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
+const API = "https://api.escuelajs.co/api/v1/products"
 
 function Home() {
+  const [products, setProducts] = useState(null)
+  // Aquí uso useEffect porque estoy consumiendo la API de Platzi Fake Store
+  useEffect(() => {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => setProducts(data))
+    .catch(error => console.log(error))
+  }, []) // El array de dependencias vacio está aquí para hacer que se ejecute solo una vez este efecto.
+
   return (
     <Layout>
       Home
-      <Card />
+      {/* Esta es la sección de los productos*/}
+      <section className="grid grid-cols-3 gap-2 w-full max-w-screen-lg place-items-center">
+        {
+          products?.map(product => {
+            // Envio por props los datos necesarios para renderizar las Cards.
+            return <Card key={product.id} category={product.category.name} image={product.category.image} title={product.title} price={product.price}/>
+          })
+        }
+      </section>
     </Layout>
   )
 }
