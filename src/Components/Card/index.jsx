ef7a2c3442551a750dc2f42e7fb6ac9c53a2f2ab
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-30 21:51:30 -0500

    Añado el componente Card con estilos básicos. Avances hasta la parte 7.

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,25 @@
+function Card() {
+  return (
+    <div className="bg-white cursor-pointer w-56 h-68 rounded-lg">
+      <figure className="relative mb-2 w-full h-4/5 rounded-lg">
+        <span className="absolute bottom-0 left-0 bg-zinc-300 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          Electronics
+        </span>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src="http://t0.gstatic.com/images?q=tbn:ANd9GcTIHAwI9f7-kNF6xsPR573FXecUF5HLJMveyeMuHEZG4z_c7Cg&s"
+          alt="Headphones"
+        />
+        <button className="absolute top-0 right-0 flex justify-center items-center rounded-full bg-white m-2 p-1 w-6 h-6">
+          <p>+</p>
+        </button>
+      </figure>
+      <p className="flex justify-between">
+        <span className="text-sm font-light">Headphones</span>
+        <span className="text-lm font-bold">$300</span>
+      </p>
+    </div>
+  )
+}
+
+export { Card }
