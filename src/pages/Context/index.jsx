e21baa51d136101da3d5d3666d8602866308ccd5
Emanuel Padilla <emanuehl159@gmail.com>
2023-06-03 16:39:55 -0500

    Añado el input para posteriormente buscar/filtrar productos. En Home. Avances hasta la parte 26.

--- src/pages/Context/index.jsx
+++ src/pages/Context/index.jsx
@@ -1,59 +1,80 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
+const API = "https://api.escuelajs.co/api/v1/products"
 
 const ShoppingCartContext = createContext()
 
-function ShoppingCartProvider({children}) {
-    // Conteo de items en el ShoppingCart
-    const [count, setCount] = useState(0)
+function ShoppingCartProvider({ children }) {
+  // Conteo de items en el ShoppingCart
+  const [count, setCount] = useState(0)
 
-    // Relacionado a ProductDetail
-    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false) // Almacena un valor para posteriormente mostrar/ocultar ProductDetail.
-    const [productToShow, setProductToShow] = useState({}) // Almacena los datos del producto que se muestra en ProductDetail.
-    function openProductDetail() {
-        setIsProductDetailOpen(true)
-    }
-    function closeProductDetail() {
-        setIsProductDetailOpen(false)
-    }
-    // ProductCart
+  // Relacionado a ProductDetail
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false) // Almacena un valor para posteriormente mostrar/ocultar ProductDetail.
+  const [productToShow, setProductToShow] = useState({}) // Almacena los datos del producto que se muestra en ProductDetail.
+  function openProductDetail() {
+    setIsProductDetailOpen(true)
+  }
+  function closeProductDetail() {
+    setIsProductDetailOpen(false)
+  }
+  // ProductCart
 
-    // Almacena en un estado, un array con los productos dentro del carrito de compras (ShoppingCart)
-    const [cartProducts, setCartProducts] = useState([])
+  // Almacena en un estado, un array con los productos dentro del carrito de compras (ShoppingCart)
+  const [cartProducts, setCartProducts] = useState([])
 
-    // CheckoutSideMenu
+  // CheckoutSideMenu
 
-    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
-    function openCheckoutSideMenu() {
-        setIsCheckoutSideMenuOpen(true)
-    }
-    function closeCheckoutSideMenu() {
-        setIsCheckoutSideMenuOpen(false)
-    }
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  function openCheckoutSideMenu() {
+    setIsCheckoutSideMenuOpen(true)
+  }
+  function closeCheckoutSideMenu() {
+    setIsCheckoutSideMenuOpen(false)
+  }
 
-    // Este estado almacena la orden cuando se hace click en Checkout en CheckoutSideMen
-    const [order, setOrder] = useState([])
+  // Este estado almacena la orden cuando se hace click en Checkout en CheckoutSideMen
+  const [order, setOrder] = useState([])
 
-    return (
-        // Exporto así el elemento para que sea un poco más facil de leer desde otros archivos, como en App/index.jsx
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow,
-            setProductToShow,
-            cartProducts,
-            setCartProducts,
-            isCheckoutSideMenuOpen,
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            order,
-            setOrder
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
+  // Product Search/Filter
+
+  const [products, setProducts] = useState(null)
+  // Aquí uso useEffect porque estoy consumiendo la API de Platzi Fake Store
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.log(error))
+  }, []) // El array de dependencias vacio está aquí para hacer que se ejecute solo una vez este efecto.
+
+  const [productSearchVaule, setProductSearchValue] = useState('')
+  console.log(productSearchVaule)
+
+  return (
+    // Exporto así el elemento para que sea un poco más facil de leer desde otros archivos, como en App/index.jsx
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+        openProductDetail,
+        closeProductDetail,
+        isProductDetailOpen,
+        productToShow,
+        setProductToShow,
+        cartProducts,
+        setCartProducts,
+        isCheckoutSideMenuOpen,
+        openCheckoutSideMenu,
+        closeCheckoutSideMenu,
+        order,
+        setOrder,
+        products,
+        setProducts,
+        productSearchVaule,
+        setProductSearchValue
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  )
 }
 
 export { ShoppingCartProvider, ShoppingCartContext }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,20 @@
-import { useState, useEffect } from "react"
+import { useContext } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
 import { ProductDetail } from "../../Components/ProductDetail"
-const API = "https://api.escuelajs.co/api/v1/products"
+import { ShoppingCartContext } from "../Context"
 
 function Home() {
-  const [products, setProducts] = useState(null)
-  // Aquí uso useEffect porque estoy consumiendo la API de Platzi Fake Store
-  useEffect(() => {
-    fetch(API)
-    .then(response => response.json())
-    .then(data => setProducts(data))
-    .catch(error => console.log(error))
-  }, []) // El array de dependencias vacio está aquí para hacer que se ejecute solo una vez este efecto.
+  const context = useContext(ShoppingCartContext)
 
   return (
     <Layout>
-      Home
+      <h1 className="font-extralight text-xl">Time to get you something great!</h1>
+      <input type="text" className="p-3 my-6 border border-slate-950 rounded-lg focus:outline-none" placeholder="Search a product" onChange={(event) => context.setProductSearchValue(event.target.value)}/>
       {/* Esta es la sección de los productos*/}
       <section className="grid grid-cols-3 gap-2 w-full max-w-screen-lg place-items-center">
         {
-          products?.map(product => {
+          context.products?.map(product => {
             // Envio por props los datos necesarios para renderizar las Cards.
             return <Card key={product.id} id={product.id} category={product.category.name} image={product.category.image} title={product.title} price={product.price} description={product.description}/>
           })
